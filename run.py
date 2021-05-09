da55cd424a8b34309185a9826d1c9a5c1ebe9b92
suraj <surajdeshmukhssd66@gmail.com>
2021-05-09 04:41:52 +0000

    deployment changes

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-import sys
 from app.application import app
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host="0.0.0.0", port=int(sys.argv[1]))
+    app.debug = False
+    app.run()
