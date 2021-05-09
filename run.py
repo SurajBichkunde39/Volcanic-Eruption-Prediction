9bcb9907a93e71de0e8ce2c5189571e1537ecc99
suraj <surajdeshmukhssd66@gmail.com>
2021-05-09 06:49:51 +0000

    deployment try

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 from app.application import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.debug = False
     app.run(host='0.0.0.0', port=port)
